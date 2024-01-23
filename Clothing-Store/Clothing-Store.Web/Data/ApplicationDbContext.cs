58b79b1ad5f10403f1c9923c1f897ff49626e950
ItsAlphaHelix <mitak4000@gmail.com>
2024-01-23 11:04:41 +0200

    Initial Commit.

---
+++ Clothing-Store/Clothing-Store.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clothing_Store.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
